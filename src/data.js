48f374f9fbaca4643b1b45a530f56ad485b1eacb
nix <john.nicholai.vigilia@gmail.com>
2022-01-15 02:39:20 +0800

    finish ecom ui

--- src/data.js
+++ src/data.js
@@ -1,23 +1,76 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "/images/model-1.jpg",
+    img: "https://images.pexels.com/photos/1485781/pexels-photo-1485781.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     title: "SUMMER SALE",
     desc: "SPECIAL OFFER 30% SALE. DESIGNER BRAND SEASON OFF IN-STORE & ONLINE FOR A LIMITED TIME",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "/images/model-2.jpg",
+    img: "https://images.pexels.com/photos/715546/pexels-photo-715546.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     title: "WINTER SALE",
     desc: "SPECIAL OFFER 30% SALE. DESIGNER BRAND SEASON OFF IN-STORE & ONLINE FOR A LIMITED TIME",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "/images/model-3.jpg",
+    img: "https://images.pexels.com/photos/720606/pexels-photo-720606.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
     title: "BLACK FRIDAY SALE",
     desc: "SPECIAL OFFER 30% SALE. DESIGNER BRAND SEASON OFF IN-STORE & ONLINE FOR A LIMITED TIME",
     bg: "f5fafd",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/965324/pexels-photo-965324.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    title: "NEW ARRIVAL!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/3687550/pexels-photo-3687550.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    title: "SUMMER STYLE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/6626075/pexels-photo-6626075.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    title: "MEN'S WEAR",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "http://assets.stickpng.com/images/580b57fbd9996e24bc43bf85.png",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://cdn.picpng.com/watches/watches-hd-38675.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
 ];
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="productlist" element={<ProductList />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="cart" element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
