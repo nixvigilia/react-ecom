9617b4e3fab663ae98d160d9cc4039c492387fc1
nix <john.nicholai.vigilia@gmail.com>
2022-01-14 08:52:43 +0800

    initial commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "/images/model-1.jpg",
+    title: "SUMMER SALE",
+    desc: "SPECIAL OFFER 30% SALE. DESIGNER BRAND SEASON OFF IN-STORE & ONLINE FOR A LIMITED TIME",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "/images/model-2.jpg",
+    title: "WINTER SALE",
+    desc: "SPECIAL OFFER 30% SALE. DESIGNER BRAND SEASON OFF IN-STORE & ONLINE FOR A LIMITED TIME",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "/images/model-3.jpg",
+    title: "BLACK FRIDAY SALE",
+    desc: "SPECIAL OFFER 30% SALE. DESIGNER BRAND SEASON OFF IN-STORE & ONLINE FOR A LIMITED TIME",
+    bg: "f5fafd",
+  },
+];
